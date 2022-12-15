efb72dfdb35f17325382202de23c1d1b3124dfda
PhoenixDev1223 <phoenixdev1223@gmail.com>
2022-12-15 10:49:41 +0800

    color schema

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,6 +29,45 @@ export default function Home() {
         <div className="w-[130px] h-[50px] bg-primary-800">800</div>
         <div className="w-[130px] h-[50px] bg-primary-900">900</div>
       </div>
+      <h1>Secondary</h1>
+      <div className="flex">
+        <div className="w-[130px] h-[50px] bg-secondary-50">50</div>
+        <div className="w-[130px] h-[50px] bg-secondary-100">100</div>
+        <div className="w-[130px] h-[50px] bg-secondary-200">200</div>
+        <div className="w-[130px] h-[50px] bg-secondary-300">300</div>
+        <div className="w-[130px] h-[50px] bg-secondary-400">400</div>
+        <div className="w-[130px] h-[50px] bg-secondary-500">500</div>
+        <div className="w-[130px] h-[50px] bg-secondary-600">600</div>
+        <div className="w-[130px] h-[50px] bg-secondary-700">700</div>
+        <div className="w-[130px] h-[50px] bg-secondary-800">800</div>
+        <div className="w-[130px] h-[50px] bg-secondary-900">900</div>
+      </div>
+      <h1>Success</h1>
+      <div className="flex">
+        <div className="w-[130px] h-[50px] bg-success-50">50</div>
+        <div className="w-[130px] h-[50px] bg-success-100">100</div>
+        <div className="w-[130px] h-[50px] bg-success-200">200</div>
+        <div className="w-[130px] h-[50px] bg-success-300">300</div>
+        <div className="w-[130px] h-[50px] bg-success-400">400</div>
+        <div className="w-[130px] h-[50px] bg-success-500">500</div>
+        <div className="w-[130px] h-[50px] bg-success-600">600</div>
+        <div className="w-[130px] h-[50px] bg-success-700">700</div>
+        <div className="w-[130px] h-[50px] bg-success-800">800</div>
+        <div className="w-[130px] h-[50px] bg-success-900">900</div>
+      </div>
+      <h1>Error</h1>
+      <div className="flex">
+        <div className="w-[130px] h-[50px] bg-error-50">50</div>
+        <div className="w-[130px] h-[50px] bg-error-100">100</div>
+        <div className="w-[130px] h-[50px] bg-error-200">200</div>
+        <div className="w-[130px] h-[50px] bg-error-300">300</div>
+        <div className="w-[130px] h-[50px] bg-error-400">400</div>
+        <div className="w-[130px] h-[50px] bg-error-500">500</div>
+        <div className="w-[130px] h-[50px] bg-error-600">600</div>
+        <div className="w-[130px] h-[50px] bg-error-700">700</div>
+        <div className="w-[130px] h-[50px] bg-error-800">800</div>
+        <div className="w-[130px] h-[50px] bg-error-900">900</div>
+      </div>
     </>
   );
 }
