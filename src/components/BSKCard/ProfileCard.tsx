67d66d13fc774e181ee6e3f06ab263f9b77acce6
Full Stack Developer <104979478+PhoenixDev1223@users.noreply.github.com>
2022-12-29 17:34:31 +0800

    BSKProfile Card Update

--- src/components/BSKCard/ProfileCard.tsx
+++ src/components/BSKCard/ProfileCard.tsx
@@ -1,4 +1,3 @@
-import { Avatar } from "../Avatar"
 import Chip from "../Chip"
 import Icon from "../Icon"
 import styles from "./BSKCard.module.css"
@@ -7,38 +6,26 @@ interface BSKProfileProps {
 
 }
 
-export default function HomeCard(props: BSKProfileProps) {
+export default function ProfileCard(props: BSKProfileProps) {
     return (
-        <div className={styles['bskcard-home']}>
-            <div className={styles['bsk-avatar']}>
-                <Avatar.CreatorAvatar size="lg" src="https://i.pravatar.cc/150?img=3" />
+        <div className={styles['bskcard-profile']}>
+            <div className={`${styles["header-free"]} flex items-center`}>
+                <Chip type="free">Free</Chip>
+                <Chip type="free" value="1 unlocked"><Icon icon="noofpeople" className="fill-shades-0 mr-[3px]" /></Chip>
             </div>
-            <div className={styles["bsk-content"]}>
-                <div className={styles["header"]}>
-                    <div className="font-extrabold text-neutral-700 text-sm">
-                        <span>Owner Name</span>
-                    </div>
-                    <div className="font-extrabold text-error-500 text-xm inline-flex items-center">
-                        <Icon icon="clock" className="mr-[5px] fill-error-500" />
-                        <span>Posted 3 days ago</span>
-                    </div>
+            <div className={styles["body-free"]}>
+                <img
+                    className="absolute w-full"
+                    src="https://picsum.photos/id/27/300/200"
+                />
+            </div>
+            <div className={styles["footer"]}>
+                <div className="font-extrabold text-error-500 text-xm inline-flex items-center">
+                    <Icon icon="clock" className="mr-[5px] fill-error-500" />
+                    <span>Posted 3 days ago</span>
                 </div>
-                <div className={styles["body"]}>
-                    <div className={styles[`thumbnail-locked`]}>
-                        <img
-                            className="absolute w-full"
-                            src="https://picsum.photos/id/27/300/200"
-                        />
-                    </div>
-                </div>
-                <div className={styles["footer"]}>
-                    <div className="flex justify-center">
-                        <Chip className="mr-[9px]" type="locked">$USD 100</Chip>
-                        <Chip type="locked" value="1 purchased"><Icon icon="noofpeople" className="fill-shades-0 mr-[3px]"/></Chip>
-                    </div>
-                    <div className="line-clamp-2 mt-[10px] font-bold text-xl text-primary-500">
-                        <span>BSK title BSK titlte BSK title BSK title BSK title BSK title BSK title</span>
-                    </div>
+                <div className="line-clamp-2 font-bold text-xl text-primary-500">
+                    <span>BSK title BSK titlte BSK title BSK title BSK title BSK title BSK title</span>
                 </div>
             </div>
         </div>
