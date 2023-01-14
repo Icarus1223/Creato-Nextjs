66feae5d072376fda4cd5573f7bbbc5fa4f7c321
Full Stack Developer <104979478+PhoenixDev1223@users.noreply.github.com>
2023-01-15 00:18:26 +0800

    quick fix

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -16,7 +16,7 @@ interface DropdownProps {
 const Dropdown: React.FC<DropdownProps> = (props) => {
     return (
         <Menu as="div" className={styles["dropdown"]}>
-            <Menu.Button className="inline-flex w-full justify-center rounded-md bg-shades-10 bg-opacity-20 px-4 py-2 text-sm font-normal text-shades-0 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+            <Menu.Button>
                 {props.trigger}
             </Menu.Button>
             <Transition
